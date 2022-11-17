97beec4452d806332d46c8e2862c729b86b2ac0d
Paulo Renato <paulorenatorf@hotmail.com>
2022-11-17 08:43:51 -0300

    created get and save user

---
+++ index.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+const express = require("express");
+var cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const Pool = require("pg").Pool;
+
+const { Client } = require("pg");
+const client = new Client({
+  user: process.env.USER,
+  host: "localhost",
+  database: process.env.DB,
+  password: process.env.PASSWORD,
+  dialect: "postgres",
+  port: 9090,
+});
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: "localhost",
+  database: process.env.DB,
+  password: process.env.PASSWORD,
+  dialect: "postgres",
+  port: 9090,
+});
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log("connected to client");
+  }
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client", err.stack);
+  }
+  client.query("SELECT NOW()", (err, result) => {
+    release();
+    if (err) {
+      return console.error("Error executing query", err.stack);
+    }
+    console.log("Connected to Database !");
+  });
+});
+
+app.get("/getusers", (req, res, next) => {
+  pool.query("Select * from users").then((users) => {
+    res.send(users.rows);
+  });
+});
+
+app.post("/saveuser", (req, res) => {
+  const { name, sobrenome, cpf, age } = req.body;
+
+  text =
+    "INSERT INTO users (name, sobrenome, cpf,age) VALUES($1, $2, $3, $4) RETURNING *";
+
+  values = [name, sobrenome, cpf, age];
+
+  client
+    .query(text, values)
+    .then((resp) => {
+      res.json(resp.rows[0]);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
+
+const server = app.listen(5000, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+});
